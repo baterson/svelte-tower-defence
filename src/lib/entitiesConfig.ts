0178168ea3090449dbd8cbfbd73d02a7537b39ef
Ivan Semochkin <ivan.s@7peakssoftware.com>
2024-10-29 12:42:07 +0700

    update sprite

--- src/lib/entitiesConfig.ts
+++ src/lib/entitiesConfig.ts
@@ -1,20 +1,43 @@
-export const SPRITE_URL = '/sprites.png';
+export const SPRITE_URL = '/sprite.png';
 
 export const entities = {
 	gun: {
 		sprite: {
-			x: -640,
-			y: -214
+			x: -645,
+			y: -537
 		},
 		width: 48,
 		height: 20
 	},
 	blade: {
 		sprite: {
-			x: -1,
-			y: -352
+			x: -6,
+			y: -675
 		},
 		width: 47,
 		height: 47
+	},
+	tower: {
+		sprite: {
+			x: -650,
+			y: -396
+		},
+		width: 86,
+		height: 71
+	},
+	enemy: {
+		animations: {
+			walk: [
+				{ x: -18, y: -127 },
+				{ x: -66, y: -127 },
+				{ x: -114, y: -127 },
+				{ x: -211, y: -127 },
+				{ x: -258, y: -127 }
+			]
+		},
+		width: 24,
+		height: 24
+		// width: 19,
+		// height: 22
 	}
 };
