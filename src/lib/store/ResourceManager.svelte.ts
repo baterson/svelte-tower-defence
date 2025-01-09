d2499ad773c3f15302a052ed60dc2812ec33c0bf
Ivan Semochkin <ivansemochkin@Ivans-MacBook-Pro.local>
2025-01-09 18:23:35 +0700

    add preload resources

--- src/lib/store/ResourceManager.svelte.ts
+++ src/lib/store/ResourceManager.svelte.ts
@@ -1,8 +1,14 @@
 import { sounds } from '$lib/sound';
 
 export class ResourceManager {
+	static instance = null;
 	resources = $state({});
 	preloaded = $state(false);
+	loadedCount = $state(0);
+	totalResources = $derived(Object.keys(sounds.effects).length + 1);
+	preloadPercent = $derived(
+		Math.min(Math.floor((this.loadedCount / this.totalResources) * 100), 100)
+	);
 
 	async preload() {
 		const loadSound = (name, url) => {
@@ -10,6 +16,7 @@ export class ResourceManager {
 				const audio = new Audio(url);
 				audio.addEventListener('canplaythrough', () => {
 					this.resources[name] = audio;
+					this.loadedCount += 1;
 					resolve();
 				});
 				audio.addEventListener('error', (error) => reject(error));
